c858775b86866a0555b45bc1734089e7ff647424
BloodJackal23 <31773884+BloodJackal23@users.noreply.github.com>
2020-10-17 20:37:11 +0200

    2020.10.28
    Implemented the type effect to the TutBot

--- Project Rhino/Assets/Scripts/Interactables/TutBot.cs
+++ Project Rhino/Assets/Scripts/Interactables/TutBot.cs
@@ -1,18 +1,76 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
 
 public class TutBot : MonoBehaviour
 {
+    [SerializeField] GameObject speechBubble;
+    [SerializeField] TextMeshPro textMesh;
+
+    [SerializeField] [TextArea(3, 10)] string message = "Hello there! I am Tutbot";
+    [SerializeField] float typeSpeed = 15f;
+    [SerializeField] bool displayMessageOnce = false;
+    bool displayedOnce = false;
+    float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speechBubble.SetActive(false);
+        textMesh.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(displayMessageOnce)
+        {
+            if(!displayedOnce)
+            {
+                if (collision.gameObject.tag == "Player")
+                {
+                    speechBubble.SetActive(true);
+                    timer = 0;
+                    StartCoroutine(TypeMessage(message));
+                    displayedOnce = true;
+                }
+            }
+        }
+        else
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                speechBubble.SetActive(true);
+                timer = 0;
+                StartCoroutine(TypeMessage(message));
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            textMesh.text = "";
+            timer = 0;
+            StopAllCoroutines();
+            //StopCoroutine(TypeMessage(message));
+            speechBubble.SetActive(false);
+        }
+    }
+
+    IEnumerator TypeMessage( string _message)
+    {
+        while(!TypeEffect.IsFinishedTyping(textMesh.text, _message))
+        {
+            timer += Time.deltaTime;
+            textMesh.text = TypeEffect.TypeText(_message, typeSpeed, timer);
+            Debug.Log("Typing...");
+            yield return null;
+        }
     }
 }
--- Project Rhino/Assets/Scripts/UI/In Game Dialogues/TypeEffect.cs
+++ Project Rhino/Assets/Scripts/UI/In Game Dialogues/TypeEffect.cs
@@ -1,35 +1,20 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public static class TypeEffect
 {
-    static float timer = 0;
-    public static bool pause = false;
-
-    public static void ResetTimer()
-    {
-        timer = 0;
-    }
-
-    public static void EndEffect(string _string)
-    {
-        timer = _string.Length;
-    }
-
     /// <summary>
     /// Types text with specified speed. Setting the speed to 0 will type the text instantly
     /// </summary>
     /// <param name="_body"></param>
     /// <param name="_typeSpeed"></param>
     /// <returns></returns>
-    public static string TypeText(string _body, float _typeSpeed)
+    public static string TypeText(string _body, float _typeSpeed, float _timePassed)
     {
         string currentString = "";
         if (_typeSpeed > 0)
         {
-            int charCounter = Mathf.Min((int)(timer * _typeSpeed), _body.Length);
+            int charCounter = Mathf.Min((int)(_timePassed * _typeSpeed), _body.Length);
             currentString = _body.Substring(0, charCounter);
-            timer += Time.deltaTime;
         }
         else
         {
