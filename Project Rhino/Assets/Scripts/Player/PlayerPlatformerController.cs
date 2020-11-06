0a7e8b69c5141d4aed5affe6f2f24948d0c23c2c
BloodJackal23 <31773884+BloodJackal23@users.noreply.github.com>
2020-11-06 13:59:19 +0200

    2020.11.3
    Controller finished

--- Project Rhino/Assets/Scripts/Player/PlayerPlatformerController.cs
+++ Project Rhino/Assets/Scripts/Player/PlayerPlatformerController.cs
@@ -5,18 +5,49 @@ public class PlayerPlatformerController : PhysicsObject
 {
     [SerializeField] float maxSpeed = 7f;
     [SerializeField] float jumpTakeOffSpeed = 7f;
+    bool canJump = true;
+
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Animator animator;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if(!spriteRenderer)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        if(!animator)
+        {
+            animator = GetComponent<Animator>();
+        }
     }
 
     protected override void ComputeVelocity()
     {
-        base.ComputeVelocity();
-        Vector2 move = Vector2.zero;
-        move.x = GetHorInput();
-
+        Vector2 moveInput = Vector2.zero;
+        moveInput.x = GetHorInput(true);
+        moveInput.y = GetVerInput(true);
+        if (moveInput.y > 0)
+        {
+            if (grounded && canJump)
+            {
+                velocity.y = jumpTakeOffSpeed;
+                canJump = false;
+            }
+        }
+        else
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y *= .5f;
+            }
+            else if(grounded)
+            {
+                canJump = true;
+            }
+        }
+        /*
         if(Input.GetButtonDown("Jump") && grounded)
         {
             velocity.y = jumpTakeOffSpeed;
@@ -27,13 +58,44 @@ public class PlayerPlatformerController : PhysicsObject
             {
                 velocity.y *= .5f;
             }
+        }*/
+
+        if (moveInput.x > 0)
+        {
+            spriteRenderer.flipX = false;
         }
-        targetVelocity = move * maxSpeed;
+        else if(moveInput.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        animator.SetBool("isJumping", !grounded);
+        animator.SetFloat("isRunning", Mathf.Abs(velocity.x) / maxSpeed);
+
+        targetVelocity = moveInput * maxSpeed;
     }
 
-    float GetHorInput()
+    float GetHorInput(bool _raw)
     {
-        float input = Input.GetAxisRaw("Horizontal");
-        return input;
+        if(_raw)
+        {
+            return Input.GetAxisRaw("Horizontal");
+        }
+        else
+        {
+            return Input.GetAxis("Horizontal");
+        }
+    }
+
+    float GetVerInput(bool _raw)
+    {
+        if (_raw)
+        {
+            return Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            return Input.GetAxis("Vertical");
+        }
     }
 }
