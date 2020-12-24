d1122236f70bd00a9481b03076d011ffb8a10ca1
BloodJackal23 <31773884+BloodJackal23@users.noreply.github.com>
2020-12-24 19:25:13 +0200

    ui_anim 2020.12.3
    UI buttons text changed to TextMeshPro

--- Project Rhino/Assets/Scripts/UI/MainMenu.cs
+++ Project Rhino/Assets/Scripts/UI/MainMenu.cs
@@ -5,8 +5,8 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] Button newGame;
     [SerializeField] Button howToPlay;
-    // Start is called before the first frame update
-    void Start()
+
+    void OnValidate()
     {
         newGame.onClick.AddListener(delegate { GameManager.instance.LoadScene("Level_01"); });
         howToPlay.onClick.AddListener(delegate { GameManager.instance.LoadScene("Tutorial"); }); //TODO: Make a tutorial level
